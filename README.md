c8daf334eb959364af894e1f76b1e832b6f74f8b
CCjiahao <13462260020@163.com>
2024-03-01 14:53:44 +0800

    远程调用

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 # Preparation
 
 1. Install dependencies:
+
 ```
 pip install -r requirements.txt
 ```
@@ -14,10 +15,20 @@ pip install -r requirements.txt
 > usage: login.py [-h] --username USERNAME [--interval INTERVAL] [--lark LARK [LARK ...]]
 > optional arguments:
 >
->  -h, --help            show this help message and exit
+> -h, --help            show this help message and exit
 >
->  --username USERNAME   username, 也就是学号
+> --username USERNAME   username, 也就是学号
 >
->  --interval INTERVAL   检查网络是否正常的间隔
+> --interval INTERVAL   检查网络是否正常的间隔
 
 为了保护你的密码安全，使用getpass库输入密码，密码也就是你idc账号的密码。
+
+# 第一次使用
+
+服务器如果完全没有网，无法联网，可以通过在自己机器上（连上校园网）执行
+
+```
+python get_login_url.py --username {{学号}} --ip {{服务器ip}}
+```
+
+便可以得到登录网址，然后在**几分钟内**服务器上请求该网址就行。
--- utils/network.py
+++ utils/network.py
@@ -1,21 +1,23 @@
-''' ssl utils '''
+"""ssl utils."""
 import requests
-from .encryption import hmac_md5, get_host_ip, get_token, get_info, get_timestamp, get_chksum
+
+from .encryption import (get_chksum, get_host_ip, get_info, get_timestamp,
+                         get_token, hmac_md5)
 
 
 def disable_requests_warnings():
-    ''' 绕过高版本SSL强制验证 '''
+    """绕过高版本SSL强制验证."""
     # pylint: disable=no-member
     requests.packages.urllib3.disable_warnings()
     requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
     try:
-        requests.packages.urllib3.contrib.pyopenssl.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+        requests.packages.urllib3.contrib.pyopenssl.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'  # noqa
     except AttributeError:
         pass
 
 
-def is_network_ok(url: str = "https://www.baidu.com/"):
-    '''' 检查网络是否能够连通 '''
+def is_network_ok(url: str = 'https://www.baidu.com/'):
+    """' 检查网络是否能够连通."""
     try:
         status = requests.get(url, timeout=3, verify=False).status_code
         return 200 <= status < 300
@@ -24,24 +26,28 @@ def is_network_ok(url: str = "https://www.baidu.com/"):
         return False
 
 
-def send_encu_login_post(username: str, password: str):
-    ''' 发送校园网登录请求 '''
+def get_encu_login_post(username: str, password: str, ip: str):
+    """发送校园网登录请求."""
     timestamp = get_timestamp()
-
-    ip = get_host_ip()
 
     token = get_token(username, ip, timestamp)
 
     password_hmac = hmac_md5(password, token)
-    password_last = "{MD5}" + password_hmac
+    password_last = '{MD5}' + password_hmac
 
     info = get_info(username, password, ip, token)
 
     chksum = get_chksum(token, username, password_hmac, ip, info)
 
-    url = f"callback=1&action=login&username={username}&password={password_last}&double_stack=0&chksum={chksum}&info={info}&ac_id=1&ip={ip}&n=200&type=1&_={timestamp}"
-    url = url.replace("{", "%7B")
-    url = url.replace("}", "%7D")
-    url = url.replace("+", "%2B")
-    url = url.replace("/", "%2F")
-    requests.get("https://login.ecnu.edu.cn/cgi-bin/srun_portal?" + url)
+    url = f'callback=1&action=login&username={username}&password={password_last}&double_stack=0&chksum={chksum}&info={info}&ac_id=1&ip={ip}&n=200&type=1&_={timestamp}'  # noqa
+    url = url.replace('{', '%7B')
+    url = url.replace('}', '%7D')
+    url = url.replace('+', '%2B')
+    url = url.replace('/', '%2F')
+    return 'https://login.ecnu.edu.cn/cgi-bin/srun_portal?' + url
+
+
+def send_encu_login_post(username: str, password: str):
+    """发送校园网登录请求."""
+    url = get_encu_login_post(username, password, get_host_ip())
+    requests.get(url)
